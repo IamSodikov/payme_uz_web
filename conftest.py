f2af76faec1768125497742681a527a774b618c0
Iamsodikov <mukhammadazizsodikov@gmail.com>
2025-04-08 21:56:39 +0500

    added headless

--- conftest.py
+++ conftest.py
@@ -6,6 +6,9 @@ import os
 def browser():
     remote_url = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
     options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Remote(command_executor=remote_url, options=options)
     driver.maximize_window()
     yield driver
